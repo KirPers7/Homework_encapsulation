fbd4d6625f77bae553933074e305f09d524f43ee
Kirill Kopachinskiy <Kirpers2018@yandex.ru>
2025-01-16 02:31:27 +0300

    Homework_encapsulation for check.

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,68 @@
+package org.skypro.skyshop;
+
+import org.skypro.skyshop.basket.ProductBasket;
+
+public class App {
+    public static void main(String[] args) {
+
+        ProductBasket productBasket = new ProductBasket();
+        ProductBasket productBasket2 = new ProductBasket();
+
+        //1. Добавление продукта в корзину.
+        System.out.println("Заполнение корзины 1");
+        productBasket.addProduct("Телевизор", 50_000);
+        productBasket.addProduct("Холодильник", 60_000);
+        System.out.println();
+        System.out.println("Заполнение корзины 2");
+        productBasket2.addProduct("Пылесос", 30_500);
+        productBasket2.addProduct("Телевизор", 50_000);
+        productBasket2.addProduct("Холодильник", 60_000);
+        productBasket2.addProduct("Мультиварка", 15_800);
+        productBasket2.addProduct("Яндекс станция", 12_300);
+        System.out.println();
+
+        //2. Добавление продукта в заполненную корзину, в которой нет свободного места.
+        System.out.println("Переполнение корзины 2");
+        productBasket2.addProduct("Микроволновая печь", 25_100);
+        System.out.println();
+
+        //3. Печать содержимого корзины с несколькими товарами.
+        System.out.println("Содержимое корзины 1:");
+        productBasket.getBasketContents();
+        System.out.println();
+        System.out.println("Содержимое корзины 2:");
+        productBasket2.getBasketContents();
+        System.out.println();
+
+        //4. Получение стоимости корзины с несколькими товарами.
+        System.out.println("Стоимость сожержимого корзины: " + productBasket.getTotalCost());
+        System.out.println();
+
+        //5. Поиск товара, который есть в корзине.
+        System.out.println("В наличии: " + productBasket.checkProductByName("Холодильник"));
+        System.out.println();
+
+        //6. Поиск товара, которого нет в корзине.
+        System.out.println("В наличии: " + productBasket.checkProductByName("Пылесос"));
+        System.out.println();
+
+        //7. Очистка корзины.
+        System.out.println("Очистка корзины.");
+        productBasket.clearBasket();
+        System.out.println();
+
+        //8. Печать содержимого пустой корзины.
+        System.out.println("Печать содержимого пустой корзины.");
+        productBasket.getBasketContents();
+        System.out.println();
+
+        //9. Получение стоимости пустой корзины.
+        System.out.println("Получение стоимости пустой корзины.");
+        System.out.println("Стоимость сожержимого корзины: " + productBasket.getTotalCost());
+        System.out.println();
+
+        //10. Поиск товара по имени в пустой корзине.
+        System.out.println("Поиск товара по имени в пустой корзине.");
+        System.out.println(productBasket.checkProductByName("Холодильник"));
+    }
+}
---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,75 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+
+    static final int LENGTH = 5;
+
+    private final Product[] products = new Product[LENGTH];
+
+    //1. Метод добавления продукта в корзину: метод принимает в себя продукт и ничего не возвращает.
+    public void addProduct(String name, int price) {
+        int count = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                products[i] = new Product(name, price);
+                break;
+            } else if (products[i] != null) {
+                count++;
+            }
+        }
+        if (count == LENGTH) {
+            System.out.println("Невозможно добавить продукт");
+        }
+    }
+
+    //2. Метод получения общей стоимости корзины: метод ничего не принимает и возвращает целое число.
+    public int getTotalCost() {
+        int totalCost = 0;
+        for (Product product : products) {
+            if (product != null) {
+                totalCost = totalCost + product.getProductPrice();
+            }
+        }
+        return totalCost;
+    }
+
+    //3. Метод, который печатает содержимое корзины: метод ничего не принимает и не возвращает,
+    // но печатает в консоль сообщение вида: <имя продукта>: <стоимость> и Итого: <общая стоимость корзины>
+    public void getBasketContents() {
+        for (Product product : products) {
+            if (product != null) {
+                System.out.println(product);
+            }
+        }
+        if (getTotalCost() == 0) {
+            System.out.println("в корзине пусто");
+        }
+        System.out.println("Итого: " + getTotalCost());
+    }
+
+    //4. Метод, проверяющий продукт в корзине по имени: метод принимает в себя строку имени и возвращает
+    //boolean в зависимости от того, есть продукт в корзине или его нет.
+    public boolean checkProductByName(String name) {
+        boolean result = false;
+        for (Product product : products) {
+            if (product != null && product.getProductName().equals(name)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    //5. Метод очистки корзины: метод ничего не принимает и очищает массив, проставляя всем его элементам null
+    public void clearBasket() {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] != null) {
+                products[i] = null;
+            }
+        }
+    }
+
+}
